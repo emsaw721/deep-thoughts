fd031af4d6fbc314ceba80c55c6665d609bebcb6
Emily Hill <emhillmed2021@gmail.com>
2023-01-03 17:23:09 -0600

    lesson 2

--- server/server.js
+++ server/server.js
@@ -7,10 +7,13 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+const { authMiddleware } = require('./utils/auth');
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
